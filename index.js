9d1fd7e992c80718e71599dc33b52aedfc44cc49
Danko Karleusa <dankomatichevich@gmail.com>
2022-02-09 16:28:49 -0800

    commit tree b7ae9b9ee6b55153e7ce9e0fc170cae1f37e75e6

---
+++ index.js
@@ -0,0 +1,57 @@
+function add() {
+    (add === (a + b))
+return (a + b)
+}
+
+function subtract() {
+    (subtract === (a - b))
+    return (a - b)
+}
+
+function multiply() {
+    (multiply === (a * b))
+    return (a * b)
+}
+
+function divide() {
+    (divide === (a / b))
+    return (a / b)
+}
+
+function add(a, b) {
+    (add === (a + b))
+    return (a + b)
+    }
+
+    function subtract(a, b) {
+        (subtract === (a - b))
+        return (a - b)
+    }
+
+    function multiply(a, b) {
+        (multiply === (a * b))
+        return (a * b)
+    }
+
+    function divide(a, b) {
+        (divide === (a / b))
+        return (a / b)
+    }
+
+    function increment(a) {
+        (increment === (a + 1))
+        return (a + 1)
+    }
+
+    function decrement(a) {
+        (decrement ===(a - 1))
+        return (a - 1)
+    }
+    
+    function makeInt(n) {
+       return parseInt(n, 10);
+    }
+
+    function preserveDecimal(n) {
+        return parseFloat(n);
+    }
